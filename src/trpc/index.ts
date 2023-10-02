f63e277aa3029ccf5bb0be36e1f8c67bee81c601
HarshKumarraghav <raghavharsh068@gmail.com>
2023-10-03 00:21:33 +0530

    infinidocs: minor updates 2

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -154,7 +154,7 @@ export const appRouter = router({
   createStripeSession: privateProcedure.mutation(async ({ ctx }) => {
     const { userId } = ctx;
 
-    const billingUrl = absoluteUrl("/dashboard/billing");
+    const billingUrl = `${process.env.VERCEL_URL}/dashboard/billing`;
 
     if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
 
