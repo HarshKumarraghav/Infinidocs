bc84839072ca7d3f97daad95f71e36424ccd30c0
HarshKumarraghav <raghavharsh068@gmail.com>
2023-11-14 23:49:38 +0530

    Privacy policy

---
+++ src/app/privacypolicy/page.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-6">Privacy Policy</h1>
+
+      <p>
+        <strong>Infinidocs</strong> is committed to protecting the privacy of
+        our users. This Privacy Policy outlines the types of personal
+        information we collect, how it is used, and the steps we take to ensure
+        your information is handled responsibly.
+      </p>
+
+      <h2 className="text-xl font-bold mt-4">Information We Collect:</h2>
+      <ul className="list-disc ml-6">
+        <li>Name</li>
+        <li>Contact information (email address, phone number)</li>
+        <li>Demographic information</li>
+        <li>Payment information</li>
+      </ul>
+
+      <h2 className="text-xl font-bold mt-4">How We Use Your Information:</h2>
+      <p>We use the collected information for the following purposes:</p>
+      <ul className="list-disc ml-6">
+        <li>To provide and improve our products and services</li>
+        <li>To personalize user experience</li>
+        <li>To process transactions</li>
+        <li>To send periodic emails</li>
+        <li>To comply with legal obligations</li>
+      </ul>
+
+      <h2 className="text-xl font-bold mt-4">Protection of Information:</h2>
+      <p>
+        We implement a variety of security measures to maintain the safety of
+        your personal information. We use industry-standard encryption methods
+        to protect sensitive information during transmission.
+      </p>
+
+      <h2 className="text-xl font-bold mt-4">Cookies:</h2>
+      <p>
+        We use cookies to enhance your experience on our site. Cookies are small
+        files that a site or its service provider transfers to your computer's
+        hard drive through your web browser (if you allow) that enables the
+        site's or service provider's systems to recognize your browser and
+        capture and remember certain information.
+      </p>
+
+      <h2 className="text-xl font-bold mt-4">Third-Party Disclosure:</h2>
+      <p>
+        We do not sell, trade, or otherwise transfer your personal information
+        to outside parties unless we provide you with advance notice or as
+        required by law.
+      </p>
+
+      <h2 className="text-xl font-bold mt-4">Third-Party Links:</h2>
+      <p>
+        Occasionally, at our discretion, we may include or offer third-party
+        products or services on our website. These third-party sites have
+        separate and independent privacy policies. We, therefore, have no
+        responsibility or liability for the content and activities of these
+        linked sites.
+      </p>
+
+      <h2 className="text-xl font-bold mt-4">Your Consent:</h2>
+      <p>By using our site, you consent to our privacy policy.</p>
+
+      <h2 className="text-xl font-bold mt-4">Changes to our Privacy Policy:</h2>
+      <p>
+        If we decide to change our privacy policy, we will post those changes on
+        this page.
+      </p>
+
+      <p className="mt-4">This privacy policy was last modified on [Date].</p>
+
+      <p className="mt-4">
+        If there are any questions regarding this privacy policy, you may
+        contact us using the information below.
+      </p>
+
+      <p className="mt-4">
+        <strong>Infinidocs</strong>
+        <br />
+        Gautam Buddha Nagar, Uttar Pradesh, India
+        <br />
+        raghavharsh068@gmail.com
+        <br />
+      </p>
+    </div>
+  );
+};
+
+export default page;
